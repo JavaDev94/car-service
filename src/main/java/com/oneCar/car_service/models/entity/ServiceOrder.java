dca75a6188cef5b2003f4df725887c5658141b62
Islombek <islombek_karimov@epam.com>
2024-12-04 12:04:00 +0500

    car service

--- src/main/java/com/oneCar/car_service/models/entity/ServiceOrder.java
+++ src/main/java/com/oneCar/car_service/models/entity/ServiceOrder.java
@@ -44,5 +44,83 @@ public class ServiceOrder {
     @JoinColumn(name = "discount_id")
     private Discount discount;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public RepairStatus getRepairStatus() {
+        return repairStatus;
+    }
+
+    public void setRepairStatus(RepairStatus repairStatus) {
+        this.repairStatus = repairStatus;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(LocalDate finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
 }
